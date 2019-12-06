93e631ef5e6f875fbca9125550fc5054003c3699
yboodhan <yboodhan@gmail.com>
2019-12-06 14:01:41 -0800

    added files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    //On play, make roll button appear
+    document.getElementById('play').addEventListener('click', () => {
+        isPlayerOneTurn = true
+        document.getElementById('message').textContent = 'Player 1 (blue), click roll to begin.'
+        document.getElementById('play').style.display = 'none'
+        document.getElementById('roll').style.display = 'inline-block'
+    })
+
+    //On roll, remove roll listener, start a player's turn and execute the game
+    document.getElementById('roll').addEventListener('click', playGame)
+})
