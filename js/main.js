d5d57cd3542ab1d3a9a68f87c32a464f81e8e9b7
yboodhan <yboodhan@gmail.com>
2019-12-06 14:21:52 -0800

    calcuator functions properly and recolors numbers accordingly, minimal css

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
+//GLOBAL VARIABLES
+let count = 0
+
+//Function that adds inputted value to the current value
+const adding = () => {
+    let amountToAdd = parseInt(document.getElementById('inputField').value)
+    count += amountToAdd
+    updateDisplay()
+}
+
+//Function that subtracts inputted value from the current value
+const subtracting = () => {
+    let amountToSubtract = parseInt(document.getElementById('inputField').value)
+    count -= amountToSubtract
+    updateDisplay()
+}
+
+//Function to update the display
+const updateDisplay = () => {
+    let countDisplay = document.getElementById('countDisplay')
+    countDisplay.innerText = count
+    
+    //Color change
+    if (count < 0) {
+        countDisplay.style.color = 'red'
+    } else {
+        countDisplay.style.color = 'black'
+    }
+}
+
+
 window.addEventListener('DOMContentLoaded', () => {
 
-    //On play, make roll button appear
-    document.getElementById('play').addEventListener('click', () => {
-        isPlayerOneTurn = true
-        document.getElementById('message').textContent = 'Player 1 (blue), click roll to begin.'
-        document.getElementById('play').style.display = 'none'
-        document.getElementById('roll').style.display = 'inline-block'
-    })
+    //On load, make the input field 1
+    document.getElementById('inputField').value = 1
 
-    //On roll, remove roll listener, start a player's turn and execute the game
-    document.getElementById('roll').addEventListener('click', playGame)
+    //Add event listeners to the buttons
+    document.getElementById('add').addEventListener('click', adding)
+    document.getElementById('subtract').addEventListener('click', subtracting)
 })
